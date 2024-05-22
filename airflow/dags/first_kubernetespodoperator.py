d2779ad785bc3a05e8d7a66a4ed8b63c6085e7d3
Antoine Brun <Antoine Brun>
2024-05-22 17:43:16 +0200

    test first kubernetespodoperator

---
+++ airflow/dags/first_kubernetespodoperator.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+with DAG(
+    dag_id='first_dag',
+    description='My first DAG created with DataScientest',
+    tags=['tutorial', 'datascientest'],
+    schedule_interval=None,
+    default_args={
+        'owner': 'airflow',
+        'start_date': days_ago(2),
+    }
+) as my_dag:
+
+    k = KubernetesPodOperator(
+        name="hello-dry-run",
+        image="debian",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        task_id="dry_run_demo",
+        do_xcom_push=True,
+    )
+    
+    k.dry_run()
